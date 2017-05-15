bae83dd04e996f22eb3b315e337a34d10cb089be
8MarkOut <wudixjh@sina.com>
2017-05-15 14:11:56 +0800

    按键可改变颜色

--- js/init.js
+++ js/init.js
@@ -4,6 +4,7 @@ keyCol[2] = "QWERTYUIOP[";
 keyCol[3] = "ASDFGHJKL;'";
 keyCol[4] = "ZXCVBNM,./";
 var black_key = "48WEYUP[AFGKLCM";
+var keyMap = new Map();
 
 window.onload = function() {
     init();
@@ -16,6 +17,7 @@ function init() {
             var tempDiv = document.createElement("div");
             var tempSpan = document.createElement("span");
             temp.appendChild(tempDiv);
+            keyMap[keyCol[i][j]] = tempDiv;
             tempDiv.appendChild(tempSpan);
             tempSpan.innerHTML = keyCol[i][j];
             tempSpan.setAttribute("class", "keyNum")
@@ -26,4 +28,42 @@ function init() {
                 tempDiv.setAttribute("class", "key gray")
         }
     }
+}
+
+function keyUp(e) {
+    var keycode = event.keyCode;
+    var realkey = getkeyValue(keycode);
+    var tempDiv = keyMap[realkey];
+    if (tempDiv != undefined) {
+        if (black_key.indexOf(realkey) >= 0)
+            tempDiv.setAttribute("class", "key deep-gray");
+        else
+            tempDiv.setAttribute("class", "key gray")
+    }
+}
+
+function keyDown(e) {
+    var keycode = event.keyCode;
+    var realkey = getkeyValue(keycode);
+    var tempDiv = keyMap[realkey];
+    if (tempDiv != undefined)
+        tempDiv.setAttribute("class", "key green");
+}
+
+document.onkeydown = keyDown;
+document.onkeyup = keyUp;
+
+function getkeyValue(keycode) {
+    var realkey;
+    switch(keycode) {
+        case 189: realkey = "-"; break;
+        case 209: realkey = "["; break;
+        case 186: realkey = ";"; break;
+        case 222: realkey = "'"; break;
+        case 188: realkey = ","; break;
+        case 190: realkey = "."; break;
+        case 191: realkey = "/"; break;
+        default: realkey = String.fromCharCode(event.keyCode);
+    }
+    return realkey;
 }
