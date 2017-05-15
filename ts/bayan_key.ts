7463350e1d4f7d03a7352382c2c647ab8ef478c1
8MarkOut <wudixjh@sina.com>
2017-05-16 00:23:37 +0800

    增加bayan_key类

---
+++ ts/bayan_key.ts
@@ -0,0 +1,34 @@
+class bayan_key {
+    private keyChar: string;
+    private keyElement: any;
+    private black_key: string = "48WEYUP[AFGKLCM";
+    constructor(keyChar: string, keyElement: any) {
+        this.keyChar = keyChar;
+        this.keyElement = keyElement;
+        this.init();
+    }
+    public getKeyChar(): string {
+        return this.keyChar;
+    }
+    public getKeyElement(): any {
+        return this.keyElement;
+    }
+    public identify(keyChar: string): boolean {
+        return this.keyChar === keyChar;
+    }
+    public changeColor(): void {
+        if (this.keyElement !== undefined)
+            this.keyElement.setAttribute("class", "key green");
+    }
+    public init(): void {
+        if (this.keyElement !== undefined) {
+            if (this.black_key.indexOf(this.keyChar) >= 0)
+                this.keyElement.setAttribute("class", "key deep-gray");
+            else
+                this.keyElement.setAttribute("class", "key gray");
+        }
+    }
+    public playSound(): void {}
+}
+
+export { bayan_key };
