77e100a4503ebfce5c1086338b8948800766fe27
8MarkOut <wudixjh@sina.com>
2017-05-16 12:36:58 +0800

    使用gulp进行TS的热编译

---
+++ gulpfile.js
@@ -0,0 +1,44 @@
+var gulp = require("gulp");
+var browserify = require("browserify");
+var source = require('vinyl-source-stream');
+var tsify = require("tsify");
+var uglify = require('gulp-uglify');
+var sourcemaps = require('gulp-sourcemaps');
+var buffer = require('vinyl-buffer');
+var paths = {
+    pages: ['src/*.html'],
+    css: ['src/css/*.css'],
+    ts: ['src/ts/*.ts']
+};
+
+gulp.task("tsc", function () {
+    return browserify({
+        basedir: '.',
+        debug: true,
+        entries: ['src/ts/init.ts'],
+        cache: {},
+        packageCache: {}
+    })
+    .plugin(tsify)
+    .bundle()
+    .pipe(source('bundle.js'))
+    .pipe(gulp.dest("dist"));
+});
+
+gulp.task("copy-css", function () {
+    return gulp.src(paths.css)
+        .pipe(gulp.dest("dist/css"));
+});
+
+gulp.task("copy-html", function () {
+    return gulp.src(paths.pages)
+        .pipe(gulp.dest("dist"));
+});
+
+gulp.task("watch", function() {
+    gulp.watch(paths.pages, ["copy-html"]);
+    gulp.watch(paths.css, ["copy-css"]);
+    gulp.watch(paths.ts, ["tsc"]);
+});
+
+gulp.task("default", ["copy-html", "copy-css", "tsc", "watch"]);
