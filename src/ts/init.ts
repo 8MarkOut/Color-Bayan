4c71182d7bfbe42a2daba72d1251a23b2b972662
Zhihui Leung <337891742@qq.com>
2017-05-25 15:04:15 +0800

    Add a module named "request" and the "loading" function

--- src/ts/init.ts
+++ src/ts/init.ts
@@ -5,13 +5,12 @@ import { Piano } from "./Piano";
 import { SoundFont } from "./SoundFont";
 import { keybdEvent } from "./keyBoardEvent";
 import { soundEvent } from "./soundEvent";
+
+import { request } from "./request";
+
 import * as $ from "jquery";
 
 let keyCol: Array<string> = ["1234567890-=", "QWERTYUIOP[]", "ASDFGHJKL;'", "ZXCVBNM,./"];
-let keyBd: Array<string>;
-let Music: Array<string>;
-let Instrument: Array<string>;
-let Mobile: Array<Array<string>>;
 
 window.onload = function(){
     init();
@@ -23,37 +22,21 @@ document.onkeydown = keybdEvent.keyDown;
 document.onkeyup = keybdEvent.keyUp;
 
 function init_button(): void {
-    $.ajax("https://www.easy-mock.com/mock/592183d59aba4141cf29581d/example/user").then(function(data){
-            keyBd = data.keyBd;
-            Music = data.Music;
-            Instrument = data.Instrument;
-            Mobile = [keyBd, Music, Instrument];
-            initDropdownMenu();
-            initMoblie();
-    });
-    
-    // It is used for test without network.
-
-    // keyBd = ["keyboardMap3", "keyboardMap4"];
-    // Music = ["M1", "M2", "M3"];
-    // Instrument = ["In1", "In2"];
-    // Mobile = [keyBd, Music, Instrument];
-    // initDropdownMenu();
-    // initMoblie();
+    request.requestButton(initDropdownMenu, initMoblie);
 }
 
 function initDropdownMenu(): void {
     let dropdown_content: any = document.getElementsByClassName("dropdown-content");
-    for (let i: number = 0; i < Mobile.length; i++) {
+    for (let i: number = 0; i < request.Mobile.length; i++) {
         let tempElement: any = dropdown_content[i];
-        for (let j: number = 0; j < Mobile[i].length; j++) {
+        for (let j: number = 0; j < request.Mobile[i].length; j++) {
             let tempLi: any = document.createElement("li");
             let tempA: any = document.createElement("a");
             tempElement.appendChild(tempLi);
             tempLi.appendChild(tempA);
-            tempA.innerHTML = Mobile[i][j];
-            if (i === 0) tempLi.onclick = () => { Bayan.getInstance().setKeybd(keyBd[j]); }
-            if (j !== Mobile[i].length - 1) {
+            tempA.innerHTML = request.Mobile[i][j];
+            if (i === 0) tempLi.onclick = () => { Bayan.getInstance().setKeybd(request.keyBd[j]); }
+            if (j !== request.Mobile[i].length - 1) {
                 let temp: any = document.createElement("li");
                 temp.setAttribute("class", "divider");
                 tempElement.appendChild(temp);
@@ -65,38 +48,26 @@ function initDropdownMenu(): void {
 function initMoblie(): void {
     let setMoblie: any = document.getElementsByClassName("collapsible-body");
 
-    for (let i: number = 0; i < Mobile.length; i++) {{
+    for (let i: number = 0; i < request.Mobile.length; i++) {{
         let tempUl: any = document.createElement("ul");
         setMoblie[i].appendChild(tempUl);
-        for (let j: number = 0; j < Mobile[i].length; j++) {
+        for (let j: number = 0; j < request.Mobile[i].length; j++) {
             let tempLi: any = document.createElement("li");
             let tempA: any = document.createElement("a");
             tempUl.appendChild(tempLi);
             tempLi.appendChild(tempA);
-            tempA.innerHTML = Mobile[i][j];
+            tempA.innerHTML = request.Mobile[i][j];
             tempA.setAttribute("class", "waves-effect");
+
             // Set keyBd
             if (i === 0) {
-                tempLi.onclick = () => { Bayan.getInstance().setKeybd(keyBd[j]); }
+                tempLi.onclick = () => { Bayan.getInstance().setKeybd(request.keyBd[j]); }
             // Get music
             } else if(i == 1) {
-                tempLi.onclick = () => {
-                    $.get("https://www.easy-mock.com/mock/592183d59aba4141cf29581d/example/query", {name: Mobile[i][j]}, function(data) {
-                        // Data is the stream of Mid file we recepted.
-                        // And here should be a function to recept the data stream and play it.
-                        console.log(data.data.name);
-                        console.log("Play Music!!!");
-                    });
-                }
+                tempLi.onclick = () => request.requestMid(j);
             // Get instrument
             } else {
-                tempLi.onclick = function() {
-                    $.get("https://www.easy-mock.com/mock/592183d59aba4141cf29581d/example/query", {name: Mobile[i][j]}, function(data) {
-                        // A function used to change the instrument should be here.
-                        console.log(data.data.name);
-                        console.log("Change instrument!!!");
-                    });
-                }
+                tempLi.onclick = () => request.requestInstrument(j);
             }
         }
     }}
---
+++ src/ts/request.ts
@@ -0,0 +1,71 @@
+import { Bayan } from "./Bayan";
+
+namespace request {
+    export let keyBd: Array<string>;
+    export let Music: Array<string>;
+    export let Instrument: Array<string>;
+    export let Mobile: Array<Array<string>>;
+
+    // To get the information of buttons and create them.
+    export let requestButton = function(init_one:any, init_two:any) {
+        loading();
+        $.ajax("https://www.easy-mock.com/mock/592183d59aba4141cf29581d/example/user").then(function(data){
+            keyBd = data.keyBd;
+            Music = data.Music;
+            Instrument = data.Instrument;
+            Mobile = [keyBd, Music, Instrument];
+            init_one();
+            init_two();
+            unload();
+        });
+
+        // The following is used for test without network.
+
+        // keyBd = ["keyboardMap3", "keyboardMap4"];
+        // Music = ["M1", "M2", "M3"];
+        // Instrument = ["In1", "In2"];
+        // Mobile = [keyBd, Music, Instrument];
+        // initDropdownMenu();
+        // initMoblie();
+    }
+
+    // Get Mid file
+    export let requestMid = function(j:number) {
+        loading();
+        $.get("https://www.easy-mock.com/mock/592183d59aba4141cf29581d/example/query", {name: Music[j]}, function(data) {
+                // Data is the stream of Mid file we recepted.
+                // And here should be a function to recept the data stream and play it.
+                console.log(data.data.name);
+                console.log("Play Music!!!");
+                unload();
+
+                // To test without server.
+                // setTimeout(function() {
+                //     unload();
+                // }, 1000);
+            });
+    }
+
+    //Get the source of instrument.
+    export let requestInstrument = function(j:number) {
+        loading();
+        $.get("https://www.easy-mock.com/mock/592183d59aba4141cf29581d/example/query", {name: Instrument[j]}, function(data) {
+            // A function used to change the instrument should be here.
+            console.log(data.data.name);
+            console.log("Change instrument!!!");
+            unload();
+        });
+    }
+
+    let loading = function() {
+        let load: any = document.getElementById("loading");
+        load.setAttribute("class", "");
+    }
+    
+    let unload = function() {
+        let load: any = document.getElementById("loading");
+        load.setAttribute("class", "invisible");
+    }
+}
+
+export { request };
